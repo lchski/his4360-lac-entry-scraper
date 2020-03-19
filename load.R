db356988fbc147c35f2d4b676ec9d3946c766897
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-03-18 21:20:43 -0400

    process list of URLs into a wide record table

--- load.R
+++ load.R
@@ -4,14 +4,16 @@ library(janitor)
 
 library(helpers)
 
+urls <- c(
+  "http://central.bac-lac.gc.ca/.redirect?app=fonandcol&id=120179&lang=eng",
+  "http://central.bac-lac.gc.ca/.redirect?app=fonandcol&id=132998&lang=eng",
+  "http://central.bac-lac.gc.ca/.redirect?app=fonandcol&id=1284188&lang=eng",
+  "http://central.bac-lac.gc.ca/.redirect?app=fonandcol&id=4938680&lang=eng",
+  "http://central.bac-lac.gc.ca/.redirect?app=fonandcol&id=4589379&lang=eng"
+)
+
 records <- tibble(
-  url = c(
-    "http://central.bac-lac.gc.ca/.redirect?app=fonandcol&id=120179&lang=eng",
-    "http://central.bac-lac.gc.ca/.redirect?app=fonandcol&id=132998&lang=eng",
-    "http://central.bac-lac.gc.ca/.redirect?app=fonandcol&id=1284188&lang=eng",
-    "http://central.bac-lac.gc.ca/.redirect?app=fonandcol&id=4938680&lang=eng",
-    "http://central.bac-lac.gc.ca/.redirect?app=fonandcol&id=4589379&lang=eng"
-  )
+  url = urls
 )
 
 r120179 <- read_html("http://central.bac-lac.gc.ca/.redirect?app=fonandcol&id=120179&lang=eng")
@@ -63,5 +65,22 @@ list(
 ) %>%
   map_dfr(extract_structured_details, .id = "id") %>%
   widen_records()
+
+z <- records %>% slice(1:2) %>% pull(url) %>% lapply(read_html)
+z %>% map_dfr(extract_structured_details, .id = "id") %>%
+  widen_records()
+
+
+search_electronic_results <- read_csv("../his4360-lac-search-results/data/out/search_electronic_results.csv") %>% clean_names
+zz <- search_electronic_results %>%
+  filter(hierarchy_level == "Fonds / Collection") %>%
+  slice(1:5) %>%
+  pull(display_url) %>%
+  lapply(read_html)
+zz %>% map_dfr(extract_structured_details, .id = "id") %>%
+  widen_records()
+zz %>% map_dfr(extract_structured_details, .id = "id") %>%
+  widen_records() %>%
+  write_csv("data/out/zz.csv")
 
   
