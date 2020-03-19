d966b679e8dc4e9160f58a6721b8fc9576221b0c
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-03-19 09:32:01 -0400

    do some fancy searching/filtering on RG extents

--- lib/analysis/misc.R
+++ lib/analysis/misc.R
@@ -10,6 +10,20 @@ gov_fonds_records %>%
   arrange(brief_title) %>%
   View()
 
+### RG extents looking for electronic
+gov_fonds_records %>%
+  extract_record_extents() %>%
+  left_join(
+    gov_fonds_details,
+    by = c("id_number" = "id_number")
+  ) %>%
+  filter(str_detect(extent, regex("electronic|computer|data|disk|disc|kb|mb|gb|tb", ignore_case = TRUE))) %>% ## "cassette is interesting, but brings lots of video/audio
+  #filter(! str_detect(extent, regex("video|audio", ignore_case = TRUE))) %>% ## we could filter on this, but pushes out some we want
+  select(id_number, brief_title, extent) %>%
+  arrange(brief_title) %>%
+  View()
+
+
 ## RGs with electronic in title [NB: not every RG with electronic records has electronic in title]
 government_fonds %>%
   filter(hierarchy_level == "Fonds / Collection") %>%
@@ -27,3 +41,5 @@ government_fonds %>%
     by = c("id_number" = "id_number")
   ) %>%
   select(id_number, brief_title, extent)
+
+
